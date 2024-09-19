85340056ed5304eda5902557affeed496660b1cc
Jose Francisco Barreno Wahab <bjose2100@gmail.com>
2024-09-19 16:19:46 -0500

    Commit sobre Megaferia Codigo

---
+++ src/Core/Narrador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jbarreno
+ */
+public class Narrador extends Persona{
+    private ArrayList<AudioLibro> libros;
+}
